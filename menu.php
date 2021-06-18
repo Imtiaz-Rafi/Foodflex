50bcb2a5ec181821d764901d102326c3fdb7e091
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2021-06-18 16:40:08 +0600

    file of fontawsome changed

--- menu.php
+++ menu.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/Logo_02.png" type="image/png">
-    <link rel="stylesheet" href="fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="fontawesome/css/all.css">
-    <script src="fontawesome/js/fontawesome.min.js"></script>
+    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../fontawesome/css/all.css">
+    <script src="../fontawesome/js/fontawesome.min.js"></script>
     <link rel="stylesheet" href="css/menue.css">
 
     <title>Menue</title>
