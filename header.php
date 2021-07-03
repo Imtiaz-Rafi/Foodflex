8883c8295f61aa83700c11f9d3b1b80a844a9765
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2021-07-04 00:53:19 +0600

    include header

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php include 'Connection.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/Logo_02.png" type="image/png">
+    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../fontawesome/css/all.css">
+    <script src="../fontawesome/js/fontawesome.min.js"></script>
+</head>
--- menu.php
+++ menu.php
@@ -2,15 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images/Logo_02.png" type="image/png">
-    <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="../fontawesome/css/all.css">
-    <script src="../fontawesome/js/fontawesome.min.js"></script>
+    <?php include 'header.php';?>
     <link rel="stylesheet" href="css/menue.css">
-
     <title>Menue</title>
 </head>
 <body>
