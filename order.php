0ecf0b5adc1bde8ba6a5e2d15521c3fe88407c37
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2021-07-23 02:30:52 +0600

    final order design complete

--- order.php
+++ order.php
@@ -8,6 +8,7 @@
 <html lang="en">
 <head>
     <?php include 'links.php';?>
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/cart.css">
     <title>Order</title>
 </head>
@@ -27,7 +28,22 @@
     <section class="grey-bg">
         <div class="container">
             <div class="container-full">
-                
+                <div class="greetings">
+                    <div class="thank_you">
+                            Thank You!<br>
+                        <i class="fas fa-heart" fa-3x></i><br>
+                    </div>
+                    <span>
+                        For Being With <a href="index.php">Foodflex.</a><br>
+                        <div class="delivery">
+                            <i class="fas fa-truck"></i>
+                            Your Delivery Is On The Way.<br>
+                        </div>
+                        We Hope To Hear From You Again Soon!
+                    </span> 
+
+                    
+                </div>
             </div>
         </div>
     </section>
