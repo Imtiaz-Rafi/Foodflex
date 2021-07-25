f9eb3583ff249ded08d1a879ea263d20d343c3ff
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2021-07-25 23:58:29 +0600

    working starts on admin panel

--- admin.php
+++ admin.php
@@ -2,45 +2,64 @@
     session_start();
     include 'Connection.php';
     include 'login/login_check.php';
-    $data = is_logged($con);
+    $admin_data = admin_logged($con);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include 'header.php';?>
-    <link rel="stylesheet" href="css/menue.css">
+    <?php include 'links.php';?>
+    <link rel="stylesheet" href="css/style.css">
     <title>Admin</title>
 </head>
 <body>
-    <div class="header">
+    <!-- HEADER -->
+    <section class="header">
         <div class="container">
             <div class="logo">
-                <a href="index.php"><img src="images/logo.png" alt="Logo" ></a>
+                <a href="index.php"><img src="images/logo.png" alt="Logo"></a>
             </div>
-            <?php 
-                if($data){ $Name = $data['Name']; ?>
-                <ul class="nav-area-user">
-                    <li><a href="profile.php"><?php echo $Name;?> </a></li>
-                </ul>
-                <ul class="logout">
-                    <li><a href="login/logout.php"> Log Out </a> </li>
-                </ul>
-                <?php }else{ ?>
-                <div class="navbar">
-                    <ul class="nav-area">
-                        <li class="fas fa-sign-in-alt login"></li>
-                        <li> <a href="login/signup.php">Sign Up</a></li>
-                        <li class="fas fa-sign-in-alt login"></li>
-                        <li><a href="login/signin.php">Sign In</a></li>
+            <div class="nav-area">
+                <?php
+                    if($admin_data){ $Name = $admin_data['Name']; ?>
+                    <ul>
+                        <li class="logout">
+                            <a href="login/logout.php">
+                                <i class="fas fa-sign-out-alt"></i>
+                                Log Out
+                            </a>
+                        </li>
+                        <li>
+                            <a href="profile.php">
+                                <i class="far fa-user"></i>
+                                <?php echo "Hi! ADMIN"?>
+                            </a>
+                        </li>
                     </ul>
-                </div>
-                <?php  }?>
+
+                <?php }else{ ?>
+                    <ul>
+                        <!-- <li>
+                            <a href="login/signup.php">
+                                <i class="fas fa-sign-in-alt"></i>
+                                Sign Up
+                            </a>
+                        </li> -->
+                        <li>
+                            <a href="admin/admin_login.php">
+                                <i class="fas fa-sign-in-alt"></i>
+                                Log In
+                            </a>
+                        </li>
+                    </ul>
+                <?php }?>
+            </div>
         </div>
-    </div>
+    </section>
+    
     <section class="bg-row text-center">
         <div class="container">
             <ul class="nav nav-tabs">
-                <li class="nav-item"><a href="#">Checkout</a></li>
+                <li class="nav-item"><a href="#">Admin</a></li>
                 <!-- <li class="nav-item"><a href="#">Info</a></li> -->
             </ul>
         </div>
@@ -50,8 +69,49 @@
             
         </div>
     </section>
-    <section class="footer">
-        <p>&copy; Copyright Foodflex.com 2021 | All rights reserved.</p>
-    </section>
+    <!-- FOOTER -->
+    <footer class="footer">
+        <div class="container">
+            <ul class="footer_top">
+                <li class="text-left" style="width:30%;">
+                    <div class="logo">
+                        <a href="index.php"><img src="images/logo.png" alt="Logo" ></a>
+                    </div>
+                </li>
+                <li class="text-center" style="width:40%;">
+                    <div class="social_media">
+                        <a href="#fb"><i class="fab fa-facebook-square"></i></a>
+                        <a href="#insta"><i class="fab fa-instagram-square"></i></a>
+                        <a href="#google"><i class="fas fa-envelope-square"></i></a>
+                    </div>
+                </li>
+                <li class="text-right" style="width:30%;">
+                    <div class="footer_login">
+                        <p>My Foodflex</p>
+                        <?php 
+                            if($admin_data){
+                                $Name = $admin_data['Name'];?>
+                                <a href="profile.php"><?= $Name?></a> / 
+                                <a href="login/logout.php">Log Out</a>
+                            <?php }else{?>
+                                <a href="login/signin.php">Sign In/Register</a>
+                            <?php }?>
+                    </div>
+                </li>
+            </ul>
+            
+            <ul class="footer_mid">
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+            <ul class="feedback">
+                <p>Help Us Serving You Better<a href="#">Give Feedback</a></p>
+                
+            </ul>
+            <p>&copy; Copyright Foodflex.com 2021 | All rights reserved.</p>
+            
+        </div>
+    </footer>
 </body>
 </html>
--- login/login_check.php
+++ login/login_check.php
@@ -14,7 +14,21 @@
         //header('location: index.php');
         return;
     }
-
+    function admin_logged($con){
+        if(isset($_SESSION['admin_id'])){
+            $ID = $_SESSION['admin_id'];
+            $sql = "SELECT * FROM admin WHERE ID='$ID'";
+            $result = $con->query($sql);
+            if($result->num_rows > 0){
+                while($row = $result->fetch_assoc()){
+                    $admin_data = $row;
+                    return $admin_data;
+                }
+            }
+        }
+        //header('location: index.php');
+        return;
+    }
 
 
 ?>
