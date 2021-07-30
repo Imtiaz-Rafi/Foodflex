c75943ddb9e1fc0aa0a49d07c5df4869055a60a7
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2021-07-30 20:33:29 +0600

    title pic updated

--- links.php
+++ links.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="images/Logo_02.png" type="image/png">
+    <link rel="shortcut icon" href="images/Logo-square-01.jpg" type="image/gif">
     <link rel="stylesheet" href="../fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="../fontawesome/css/all.css">
     <script src="../fontawesome/js/fontawesome.min.js"></script>
