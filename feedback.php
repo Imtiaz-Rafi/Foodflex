416f8b607d6e9fd74c17259e97b7337fab3a10aa
Imtiaz Rafi <imtiazrafi199918@gmail.com>
2021-08-07 23:29:15 +0600

    feedback database added

--- feedback.php
+++ feedback.php
@@ -9,7 +9,7 @@
 <head>
     <?php include 'links.php';?>
     <link rel="stylesheet" href="css/style.css">
-    <title>Foodflex</title>
+    <title>Feedback</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
     <!-- Optional theme -->
@@ -19,14 +19,44 @@
     <link rel="stylesheet" href="css/form.css" >
     <script src="form.js"></script>
 </head>
-<body class="feedback">
-    <!-- HEADER -->
-    <?php  // include 'header.php'; ?>
+<body class="feedback-body">
+    <?php
+        $ID = $Name = $Email =  $Del_Time = $Food_quality = $Hospitality = $Description = "";
+        if($_SERVER['REQUEST_METHOD']=='POST'){
+            $Name = $_REQUEST['name'];
+            $Email = $_REQUEST['email'];
+            $Del_Time = $_REQUEST['promise_time'];
+            $Food_quality = $_REQUEST['food_hot'];
+            $Hospitality = $_REQUEST['friendly'];
+            $Description = $_REQUEST['description'];
+            $sql = "INSERT INTO feedback(ID,Name,Email,Del_time,Food_quality,Hospitality,Description)
+            VALUES(0,'$Name','$Email','$Del_Time','$Food_quality','$Hospitality','$Description')";
+            $result = $con->query($sql);
+            if($result){
+                header('location: feedback.php?id=1');
+            }else{
+                echo "Something is wrong";
+            }
+        }
+
+
+
+
+    ?>
     
     <!-- BODY -->
     <section class="grey-bg">
         <div class="container">
             <div class="imagebg"></div>
+                
+                    <?php
+                        if(isset($_REQUEST['id']) && $_REQUEST['id']==1){ ?>
+                            <div class="feedback-response">
+                            <?= "Thanks For Your Feedback";?>
+                        
+                            </div>
+                    <?php } ?>
+                
                 <div class="row " style="margin-top: 50px">
                     <div class="col-md-6 col-md-offset-3 form-container">
                         <h2> <u>Feedback Form<u> </h2> 
@@ -36,7 +66,7 @@
                                     <label>Did you receive the delivery within promise time?</label>
                                     <p>
                                         <label class="radio-inline">
-									        <input type="radio" name="promise_time" id="promise_time" value="Yes" >
+									        <input type="radio" name="promise_time" id="promise_time" value="Yes" checked>
                                             Yes 
                                         </label>
                                    
@@ -54,7 +84,7 @@
 								    <label>Did you receive your food hot?</label>
                                     <p>
                                         <label class="radio-inline">
-                                            <input type="radio" name="food_hot" id="food_hot" value="Yes" >
+                                            <input type="radio" name="food_hot" id="food_hot" value="Yes" checked>
                                             Yes 
                                         </label>
                                     
@@ -70,7 +100,7 @@
                                     <label>Was the service hospitable and friendly?</label>
                                     <p>
                                         <label class="radio-inline">
-                                            <input type="radio" name="friendly" id="friendly" value="Yes" >
+                                            <input type="radio" name="friendly" id="friendly" value="Yes" checked>
                                             Yes 
                                         </label>
                                     
@@ -84,7 +114,7 @@
                             <div class="row">
                                 <div class="col-sm-12 form-group">
                                     <label for="comments"> Tell us more:</label>
-                                    <textarea class="form-control" type="textarea" name="comments" id="comments" placeholder="Tell us more" maxlength="6000" rows="7"></textarea>
+                                    <textarea class="form-control" type="textarea" name="description" id="description" placeholder="Tell us more" maxlength="6000" rows="7"></textarea>
                                 </div>
                             </div>
                             <div class="row">
@@ -103,15 +133,13 @@
                                 </div>
                             </div>
                         </form>
-                    <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Posted your feedback successfully!</h3> </div>
-                    <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
+                    <!-- <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Posted your feedback successfully!</h3> </div>
+                    <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div> -->
                 </div>
             </div>
         </div>
     </section>
 
-    <!-- FOOTER -->
-    <?php // include 'footer.php'?>
     
 </body>
 </html>
